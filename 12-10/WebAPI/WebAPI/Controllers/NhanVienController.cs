8f6d5fbcb526263dd4757ec5ba9c5dcb31b517ee
Thanh Bằng <nguyenthanhbang9.vn@gmail.com>
2023-10-12 12:10:59 +0700

    WebAPI

---
+++ 12-10/WebAPI/WebAPI/Controllers/NhanVienController.cs
@@ -0,0 +1,113 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+using WebAPI.Models;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NhanVienController : ControllerBase
+    {
+        private string filePath = @"D:\Intern\GitHub\repo\InternGonSa\12-10\WebAPI\WebAPI\ListNhanVien.json";
+
+        // GET: api/NhanVien
+        [HttpGet]
+        public IActionResult GetNhanViens()
+        {
+            try
+            {
+                string json = System.IO.File.ReadAllText(filePath);
+                var nhanViens = JsonSerializer.Deserialize<List<NhanVien>>(json);
+                return Ok(nhanViens);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        // GET: api/NhanVien/NV001
+        [HttpGet("{maNhanVien}")]
+        public IActionResult GetNhanVien(string maNhanVien)
+        {
+            try
+            {
+                string json = System.IO.File.ReadAllText(filePath);
+                var nhanViens = JsonSerializer.Deserialize<List<NhanVien>>(json);
+                var nhanVien = nhanViens.FirstOrDefault(nv => nv.MaNhanVien == maNhanVien);
+                if (nhanVien == null)
+                    return NotFound("Không tìm thấy nhân viên");
+                return Ok(nhanVien);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        // POST: api/NhanVien
+        [HttpPost]
+        public IActionResult CreateNhanVien([FromBody] NhanVien nhanVien)
+        {
+            try
+            {
+                string json = System.IO.File.ReadAllText(filePath);
+                var nhanViens = JsonSerializer.Deserialize<List<NhanVien>>(json);
+                nhanViens.Add(nhanVien);
+                System.IO.File.WriteAllText(filePath, JsonSerializer.Serialize(nhanViens));
+                return Ok("Nhân viên đã được thêm vào danh sách.");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        // PUT: api/NhanVien/NV001
+        [HttpPut("{maNhanVien}")]
+        public IActionResult UpdateNhanVien(string maNhanVien, [FromBody] NhanVien updatedNhanVien)
+        {
+            try
+            {
+                string json = System.IO.File.ReadAllText(filePath);
+                var nhanViens = JsonSerializer.Deserialize<List<NhanVien>>(json);
+                var nhanVien = nhanViens.FirstOrDefault(nv => nv.MaNhanVien == maNhanVien);
+                if (nhanVien == null)
+                    return NotFound("Không tìm thấy nhân viên");
+                nhanVien.TenNhanVien = updatedNhanVien.TenNhanVien;
+                nhanVien.NgaySinh = updatedNhanVien.NgaySinh;
+                nhanVien.Email = updatedNhanVien.Email;
+                nhanVien.SDT = updatedNhanVien.SDT;
+                nhanVien.DiaChi = updatedNhanVien.DiaChi;
+                System.IO.File.WriteAllText(filePath, JsonSerializer.Serialize(nhanViens));
+                return Ok("Nhân viên đã được cập nhật.");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        // DELETE: api/NhanVien/NV001
+        [HttpDelete("{maNhanVien}")]
+        public IActionResult DeleteNhanVien(string maNhanVien)
+        {
+            try
+            {
+                string json = System.IO.File.ReadAllText(filePath);
+                var nhanViens = JsonSerializer.Deserialize<List<NhanVien>>(json);
+                var nhanVien = nhanViens.FirstOrDefault(nv => nv.MaNhanVien == maNhanVien);
+                if (nhanVien == null)
+                    return NotFound("Không tìm thấy nhân viên");
+                nhanViens.Remove(nhanVien);
+                System.IO.File.WriteAllText(filePath, JsonSerializer.Serialize(nhanViens));
+                return Ok("Nhân viên đã được xóa khỏi danh sách.");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
--- 12-10/WebAPI/WebAPI/Program.cs
+++ 12-10/WebAPI/WebAPI/Program.cs
@@ -7,6 +7,8 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddCors(options => options.AddDefaultPolicy(policy => policy.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod()));
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
